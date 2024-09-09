a829e1dfd4693b35b0e770e8adc8f3c7bca947ab
Ángel Díaz <diazangel9311@gmail.com>
2024-09-08 21:16:05 -0500

    Implement all CRUD for income

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -2,6 +2,7 @@ class IncomesController < ApplicationController
   include Api::ResponseBuilder
 
   before_action :set_budget
+  before_action :set_income, only: %i[show update destroy]
 
   def index
     income_list = @budget.incomes.order(created_at: :desc)
@@ -9,6 +10,20 @@ class IncomesController < ApplicationController
     response = build_successful_response(:SUCCESS, data:)
 
     render json: response
+  end
+
+  def show
+    if @income.nil?
+      errors = ["income not found with id #{params[:id]}"]
+      response = build_error_response(:ERROR_NOT_FOUND, errors:)
+
+      render json: response, status: :not_found
+    else
+      data = { income: @income.serialized_hash }
+      response = build_successful_response(:SUCCESS, data:)
+
+      render json: response
+    end
   end
 
   def create
@@ -26,10 +41,42 @@ class IncomesController < ApplicationController
     end
   end
 
+  def update
+    if @income.update(income_params)
+      data = { income: @income.serialized_hash }
+      response = build_successful_response(:SUCCESS_UPDATED, data:)
+
+      render json: response
+    else
+      errors = @income.errors.full_messages
+      response = build_error_response(:ERROR_NOT_UPDATED, errors:)
+
+      render json: response, status: :bad_request
+    end
+  end
+
+  def destroy
+    if @income.destroy
+      data = { income: @income.serialized_hash }
+      response = build_successful_response(:SUCCESS_DELETED, data:)
+
+      render json: response
+    else
+      errors = @income.errors.full_messages
+      response = build_error_response(:ERROR_NOT_DELETED, errors:)
+
+      render json: response, status: :bad_request
+    end
+  end
+
   private
 
   def set_budget
     @budget = Budget.find_by(uid: params[:budget_uid])
+  end
+
+  def set_income
+    @income = Income.find_by(id: params[:id])
   end
 
   def income_params
