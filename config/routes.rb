2e70fab68f256cd30d51ab56e9939567b81fb910
Ángel Díaz <diazangel9311@gmail.com>
2024-09-11 18:56:36 -0500

    Remove api scope

--- config/routes.rb
+++ config/routes.rb
@@ -11,15 +11,13 @@ Rails.application.routes.draw do
     end
   end
 
-  scope :api do
-    # Budgets
-    get 'budgets/current', to: 'budgets#current', as: :current_budget
-    resources :budgets, param: :uid, only: %i[index show create] do
-      resources :incomes, only: %i[index show create update destroy]
-      resources :expenses, only: %i[index show create update destroy]
-      collection do
-        resources :transaction_types, only: %i[index create update destroy]
-      end
+  # Budgets
+  get 'budgets/current', to: 'budgets#current', as: :current_budget
+  resources :budgets, param: :uid, only: %i[index show create] do
+    resources :incomes, only: %i[index show create update destroy]
+    resources :expenses, only: %i[index show create update destroy]
+    collection do
+      resources :transaction_types, only: %i[index create update destroy]
     end
   end
 end
