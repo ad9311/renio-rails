d39b91d4359c9172c54319e7aba3610ce82edc46
Ángel Díaz <ad9311@proton.me>
2024-10-06 17:34:29 -0500

    Fix not responding with 404 when current budget not found

--- app/controllers/budget_accounts_controller.rb
+++ app/controllers/budget_accounts_controller.rb
@@ -5,18 +5,22 @@ class BudgetAccountsController < ApplicationController
 
   def user
     current_budget = Budget.current(@budget_account)
-    last_expense = current_budget.expenses.order(:created_at).last
-    data = {
-      budget_account: {
-        current_budget: {
-          uid: current_budget.uid,
-          balance: current_budget.balance.to_f,
-          last_expense_amount: last_expense.nil? ? nil : last_expense.amount.to_f
+    if current_budget.nil?
+      errors = ['user has no current budget']
+      render json: build_error_response(:ERROR_NOT_FOUND, errors:)
+    else
+      last_expense = current_budget.expenses.order(:created_at).last
+      data = {
+        budget_account: {
+          current_budget: {
+            uid: current_budget.uid,
+            balance: current_budget.balance.to_f,
+            last_expense_amount: last_expense.nil? ? nil : last_expense.amount.to_f
+          }
         }
       }
-    }
-    response = build_successful_response(:SUCCESS, data:)
-    render json: response
+      render json: build_successful_response(:SUCCESS, data:)
+    end
   end
 
   private
